1e24fb658db7041a7a733d259bd41144d25c695b
Daemon Morrison-Eagan <crafterdaemon@gmail.com>
2023-12-15 23:24:13 -0500

    texture changes

--- Projectiles/NoProj.cs
+++ Projectiles/NoProj.cs
@@ -16,14 +16,7 @@ namespace RagnarokMod.Projectiles
 
         public override void SetBardDefaults()
         {
-            ((Entity)((ModProjectile)this).Projectile).width = 0;
-            ((Entity)((ModProjectile)this).Projectile).height = 0;
-            ((ModProjectile)this).Projectile.aiStyle = 0;
-            ((ModProjectile)this).Projectile.scale = 0f;
-            ((ModProjectile)this).Projectile.friendly = true;
-            ((ModProjectile)this).Projectile.penetrate = 1;
-            ((ModProjectile)this).Projectile.timeLeft = 0;
-            ((ModProjectile)this).AIType = 0;
+            ((Entity)((ModProjectile)this).Projectile).Size = new();
         }
     }
 }
