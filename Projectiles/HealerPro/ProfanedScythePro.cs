146f362da11bac78bb8e4b487a20f579e6265886
frogleader1969 <79435073+frogleader1969@users.noreply.github.com>
2024-01-30 15:00:43 +0530

    Create ProfanedScythePro.cs

---
+++ Projectiles/HealerPro/ProfanedScythePro.cs
@@ -0,0 +1,25 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThoriumMod.Projectiles.Scythe;
+
+namespace RagnarokMod.Projectiles.HealerPro
+{
+    public class ProfanedScythePro : ScythePro
+    {
+        public override void SafeSetStaticDefaults()
+        {
+        }
+
+        public override void SafeSetDefaults()
+        {
+            base.Projectile.Size = new Vector2(160f);
+            dustOffset = new Vector2(-35f, 7f);
+            dustCount = 4;
+            dustType = 236;
+        }
+    }
+}
