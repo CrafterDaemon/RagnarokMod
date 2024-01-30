69d4773440198d1cbba6ef8d6e7c787f6033726d
Daemon Morrison-Eagan <crafterdaemon@gmail.com>
2024-01-30 10:42:07 -0500

    updated scythe example

--- Items/HealerItems/MarbleScythe.cs
+++ Items/HealerItems/MarbleScythe.cs
@@ -10,6 +10,7 @@ using ThoriumMod.Projectiles.Scythe;
 using Terraria.ID;
 using RagnarokMod.Items.Materials;
 using RagnarokMod.Projectiles.HealerPro;
+using ThoriumMod;
 
 namespace RagnarokMod.Items.HealerItems
 {
@@ -23,6 +24,21 @@ namespace RagnarokMod.Items.HealerItems
         public override void SetDefaults()
         {
             SetDefaultsToScythe();
+            //this is what SetDefaultsToScythe does
+            scytheSoulCharge = 0;
+            isHealer = true;
+            base.Item.DamageType = ThoriumDamageBase<HealerDamage>.Instance;
+            base.Item.noMelee = true;
+            base.Item.noUseGraphic = true;
+            base.Item.autoReuse = true;
+            base.Item.useTime = 22;
+            base.Item.useAnimation = 22;
+            base.Item.maxStack = 1;
+            base.Item.knockBack = 6.5f;
+            base.Item.useStyle = 1;
+            base.Item.UseSound = SoundID.Item1;
+            base.Item.shootSpeed = 0.1f;
+            //end
             base.Item.damage = 18;
             scytheSoulCharge = 2;
             base.Item.width = 54;
--- Projectiles/HealerPro/MarbleScythePro.cs
+++ Projectiles/HealerPro/MarbleScythePro.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThoriumMod;
 using ThoriumMod.Projectiles.Scythe;
 
 namespace RagnarokMod.Projectiles.HealerPro
@@ -16,7 +17,22 @@ namespace RagnarokMod.Projectiles.HealerPro
 
         public override void SafeSetDefaults()
         {
-            base.Projectile.Size = new Vector2(126f);
+            Projectile.aiStyle = 0;
+            Projectile.light = 0.2f;
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            Projectile.ownerHitCheck = true;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 26;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 10;
+            Projectile.DamageType = ThoriumDamageBase<HealerDamage>.Instance;
+            fadeOutTime = 10;
+            fadeOutSpeed = 30;
+            rotationSpeed = 0.25f;
+            scytheCount = 2;
+            Projectile.Size = new Vector2(126f);
             dustOffset = new Vector2(-28f, 6f);
             dustCount = 4;
             dustType = 236;
