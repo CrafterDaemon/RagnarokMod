c1938c523496ac6c1d34349673a15b0f3f5150a7
FrogLeader <ryanbansriyar@gmail.com>
2024-02-01 18:17:54 +0530

    message

---
+++ Projectiles/HealerPro/AstralScytheStarPro.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using ThoriumMod;
+
+namespace RagnarokMod.Projectiles.HealerPro
+{
+    public class AstralScytheStarPro : ModProjectile, ILocalizedModType
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 34;
+            Projectile.height = 34;
+            Projectile.friendly = true;
+            Projectile.DamageType = ThoriumDamageBase<HealerDamage>.Instance;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.extraUpdates = 2;
+            Projectile.timeLeft = 500;
+            Projectile.ignoreWater = true;
+        }
+    }
+}
