551eb4d64928b8a3d876396250c2b77c0f46ae70
FrogLeader <ryanbansriyar@gmail.com>
2024-05-06 16:33:08 +0530

    added jelly slicer

---
+++ Items/HealerItems/Scythes/JellySlicer.cs
@@ -0,0 +1,53 @@
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using ThoriumMod.Items.HealerItems;
+using CalamityMod;
+using CalamityMod.Items.Materials;
+using RagnarokMod.Projectiles.HealerPro.Scythes;
+
+namespace RagnarokMod.Items.HealerItems.Scythes
+{
+    public class JellySlicer : ScytheItem
+    {
+        public override void SetStaticDefaults()
+        {
+            SetStaticDefaultsToScythe();
+        }
+
+        public override void SetDefaults()
+        {
+            SetDefaultsToScythe();
+            base.Item.damage = 47;
+            scytheSoulCharge = 2;
+            base.Item.width = 48;
+            base.Item.height = 44;
+            base.Item.value = Item.sellPrice(0, 28, 0);
+            base.Item.rare = ItemRarityID.Orange;
+            base.Item.shoot = ModContent.ProjectileType<GelScythePro1>();
+            base.Item.shootSpeed = 0.1f;
+        }
+         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+            Vector2 velocity2 = player.Center - Main.MouseWorld;
+            Vector2 vel = velocity2.SafeNormalize(Vector2.UnitX);
+            float multiplier = -14f;
+
+            Projectile.NewProjectileDirect(source, position, vel * multiplier, ModContent.ProjectileType<GelScythePro2>(), damage, knockback, player.whoAmI);
+
+            
+    
+			return false;
+        
+		}
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<PurifiedGel>(), 12);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
---
+++ Projectiles/HealerPro/Scythes/GelScythePro2.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ThoriumMod;
+using ThoriumMod.Projectiles.Scythe;
+
+namespace RagnarokMod.Projectiles.HealerPro.Scythes
+{
+    public class GelScythePro2 : ModProjectile, ILocalizedModType
+    {
+        public override void SetStaticDefaults()
+        {
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.aiStyle = 0;
+            Projectile.light = 0.5f;
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.ownerHitCheck = true;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = 3;
+            Projectile.timeLeft = 120;
+            Projectile.alpha = 50;
+            Projectile.DamageType = ThoriumDamageBase<HealerDamage>.Instance;
+            Projectile.Size = new Vector2(24f, 10f);
+            
+        }
+        public override void AI() {
+            Projectile.rotation = Projectile.velocity.ToRotation();
+            
+		}
+        public override bool OnTileCollide(Vector2 oldVelocity) {
+			Projectile.penetrate--;
+			if (Projectile.penetrate <= 0) {
+				Projectile.Kill();
+			}
+			else {
+				Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+				SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+				if (Math.Abs(Projectile.velocity.X - oldVelocity.X) > float.Epsilon) {
+					Projectile.velocity.X = -oldVelocity.X;
+				}
+				if (Math.Abs(Projectile.velocity.Y - oldVelocity.Y) > float.Epsilon) {
+					Projectile.velocity.Y = -oldVelocity.Y;
+				}
+			}
+
+			return false;
+		}
+    }
+}
