27323744ac90ec4f5ce7f87ed4a507c9e00e9d1f
Daemon Morrison-Eagan <crafterdaemon@gmail.com>
2024-05-07 04:06:04 -0400

    thanatos scythe, astral scythe rename

---
+++ Projectiles/HealerPro/Scythes/AstralRipperPro.cs
@@ -0,0 +1,48 @@
+﻿using CalamityMod.Dusts;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+using ThoriumMod;
+using ThoriumMod.Projectiles.Scythe;
+
+namespace RagnarokMod.Projectiles.HealerPro.Scythes
+{
+    public class AstralRipperPro : ScythePro2
+    {
+        public override void SafeSetStaticDefaults()
+        {
+        }
+
+        public override void SafeSetDefaults()
+        {
+            Projectile.aiStyle = 0;
+            Projectile.light = 0.5f;
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            Projectile.ownerHitCheck = true;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 26;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 10;
+            Projectile.DamageType = ThoriumDamageBase<HealerDamage>.Instance;
+            fadeOutTime = 10;
+            fadeOutSpeed = 30;
+            rotationSpeed = 0.25f;
+            scytheCount = 2;
+            Projectile.Size = new Vector2(222f,210f);
+
+            dustOffset = new Vector2(0f,30f);
+            dustCount = 4;
+            dustType = ModContent.DustType<AstralBlue>();
+            dustType2 = ModContent.DustType<AstralOrange>();
+
+        }
+    }
+}
