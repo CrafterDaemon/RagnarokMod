f2a540a71896a5bb1e32c6feb88cc1bcf239e5ef
Daemon Morrison-Eagan <crafterdaemon@gmail.com>
2024-05-10 10:50:00 -0400

    nightfallen legs fix

--- obj/Release/net8.0/RagnarokMod.AssemblyInfo.cs
+++ obj/Release/net8.0/RagnarokMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RagnarokMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a14ed520b2edb67c4f9aaa7320e9dcb68ef9583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RagnarokMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RagnarokMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
