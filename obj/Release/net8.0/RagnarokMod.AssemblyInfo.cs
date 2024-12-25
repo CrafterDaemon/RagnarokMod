343cd975679a952c1a7712d22dbc7cab20d5cb6f
Daemon Morrison-Eagan <crafterdaemon@gmail.com>
2024-12-25 00:35:57 -0500

    forgot to update mod version...

--- obj/Release/net8.0/RagnarokMod.AssemblyInfo.cs
+++ obj/Release/net8.0/RagnarokMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RagnarokMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff2a8f9f1ff0454d14cdd9d16b0aded0a8cdcce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e5da17ed2ae5534ef762f3d8e35e3a16bf2040")]
 [assembly: System.Reflection.AssemblyProductAttribute("RagnarokMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RagnarokMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
