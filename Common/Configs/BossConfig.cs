e3e4dc5817848eaa02d1499d5767952e1b24639e
DerPjoetrek <develop@pwever.de>
2025-07-28 23:30:09 +0200

    improved compatibility with thorium bosses reworked, added new config options, fixed the thrower reforge bug

---
+++ Common/Configs/BossConfig.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace RagnarokMod.Common.Configs
+{
+	public enum ThoriumBossRework_selection_mode
+		{
+			Off,
+			Ragnarok,
+			ThoriumBossRework
+		}
+	
+    public class BossConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+		[Header("BossChanges")]
+	   
+		[DefaultValue(true)]
+		[Label("Boss Stat Tweak")]
+		[Tooltip("Select, if Thorium Bosses receive stat tweaks (Attack Damage, Health, Defense Damage, Damage Reduction) for Calamity scaling")]
+		public bool bossstatstweak;
+	   
+		[DefaultValue(ThoriumBossRework_selection_mode.Ragnarok)]
+        [ReloadRequired]
+		[Label("Thorium Boss Rush")]
+		[Tooltip("Select, which bossrush mode should be used. Off disables bossrush.")]
+        public ThoriumBossRework_selection_mode bossrush;
+	   
+		[DefaultValue(ThoriumBossRework_selection_mode.Ragnarok)]
+        [ReloadRequired]
+		[Label("Grand Thunderbird AI")]
+		[Tooltip("Select, which AI rework should be used. Off for default behaviour")]
+        public ThoriumBossRework_selection_mode bird;
+		
+		[DefaultValue(ThoriumBossRework_selection_mode.Ragnarok)]
+        [ReloadRequired]
+		[Label("Queen Jellyfish AI")]
+		[Tooltip("Select, which AI rework should be used. Off for default behaviour")]
+        public ThoriumBossRework_selection_mode jelly;
+		
+    }
+}
---
+++ Utils/ThoriumRogueClass.cs
@@ -0,0 +1,28 @@
+using System;
+using CalamityMod;
+using Terraria.ModLoader;
+
+namespace RagnarokMod.Utils
+{
+	public class ThoriumRogueClass: DamageClass
+	{
+		public override bool GetPrefixInheritance(DamageClass damageClass)
+		{
+			return damageClass == DamageClass.Ranged;
+		}
+		
+		public override bool GetEffectInheritance(DamageClass damageClass)
+		{
+			return (damageClass == ModContent.GetInstance<RogueDamageClass>()) || (damageClass == DamageClass.Throwing);
+		}
+		
+		public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+		{
+			if (damageClass == DamageClass.Throwing || damageClass == DamageClass.Generic || damageClass == ModContent.GetInstance<RogueDamageClass>())
+			{
+				return StatInheritanceData.Full;
+			}
+			return StatInheritanceData.None;
+		}
+	}
+}
