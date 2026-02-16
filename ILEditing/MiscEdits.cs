facfb42a00ebc8f7f15d582f688ae41a45f47a07
DerPjoetrek <develop@pwever.de>
2026-02-16 19:43:18 +0100

    Minor error fix

--- ILEditing/MiscEdits.cs
+++ ILEditing/MiscEdits.cs
@@ -18,25 +18,22 @@ namespace RagnarokMod.ILEditing
         private static int scareCooldown = 0;
         public delegate void RefAction<T>(ref T value);
 
-        public override void OnModLoad()
-        {
-            On_SoundEngine.PlaySound_refSoundStyle_Nullable1_SoundUpdateCallback += (orig, ref style, position, callback) =>
-            {
-                Player player = Main.LocalPlayer;
-                if (player.active && scareCooldown <= 0)
-                {
-                    foreach (Item item in player.inventory)
-                    {
-                        if (item.type == ModContent.ItemType<PrimalTerror>() && item.favorited && Main.rand.NextBool(10000))
-                        {
-                            SoundStyle scareStyle = new SoundStyle("CalamityMod/Sounds/Custom/Scare");
-                            scareCooldown = 600;
-                            return orig(ref scareStyle, position, callback);
-                        }
-                    }
-                }
-                return orig(ref style, position, callback);
-            };
+        public override void OnModLoad(){
+			On_SoundEngine.PlaySound_refSoundStyle_Nullable1_SoundUpdateCallback +=
+			(On_SoundEngine.orig_PlaySound_refSoundStyle_Nullable1_SoundUpdateCallback orig, ref SoundStyle style, Vector2? position, SoundUpdateCallback callback) =>
+			{
+				Player player = Main.LocalPlayer;
+				if (player.active && scareCooldown <= 0){
+					foreach (Item item in player.inventory){
+						if (item.type == ModContent.ItemType<PrimalTerror>() && item.favorited && Main.rand.NextBool(10000)){
+							SoundStyle scareStyle = new SoundStyle("CalamityMod/Sounds/Custom/Scare");
+							scareCooldown = 600;
+							return orig(ref scareStyle, position, callback);
+						}
+					}
+				}
+				return orig(ref style, position, callback);
+			};
         }
     }
 }
