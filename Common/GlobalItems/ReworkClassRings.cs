bc6c65c114c0e790f4889ac4f08f6560cd575f07
DerPjoetrek <develop@pwever.de>
2026-09-24 01:52:54 +0200

    Redone the ring rework

--- Common/GlobalItems/ReworkClassRings.cs
+++ Common/GlobalItems/ReworkClassRings.cs
@@ -16,60 +16,49 @@ namespace RagnarokMod.Common.GlobalItems
             item.type == ModContent.ItemType<AquamarineRing>() ||
             item.type == ModContent.ItemType<SapphireRing>() ||
             item.type == ModContent.ItemType<AmethystRing>() ||
+			item.type == ModContent.ItemType<DiamondRing>() ||
             item.type == ModContent.ItemType<TheRing>();
 
         public override void UpdateAccessory(Item item, Player player, bool hideVisual)
         {
             if (item.type == ModContent.ItemType<OpalRing>())
             {
-                player.GetDamage(ThoriumDamageBase<BardDamage>.Instance).Flat -= 1;
-
                 player.GetDamage(ThoriumDamageBase<BardDamage>.Instance) += 0.03f;
             }
             else if (item.type == ModContent.ItemType<RubyRing>())
             {
-                player.GetDamage(DamageClass.Melee).Flat -= 1f;
-
                 player.GetDamage(DamageClass.Melee) += 0.03f;
             }
             else if (item.type == ModContent.ItemType<AmberRing>())
             {
-                player.GetDamage(DamageClass.Summon).Flat -= 1f;
-
                 player.GetDamage(DamageClass.Summon) += 0.03f;
             }
             else if (item.type == ModContent.ItemType<TopazRing>())
             {
-                player.GetDamage(DamageClass.Throwing).Flat -= 1f;
-
                 player.GetDamage(DamageClass.Throwing) += 0.03f;
             }
             else if (item.type == ModContent.ItemType<EmeraldRing>())
             {
-                player.GetDamage(DamageClass.Ranged).Flat -= 1f;
-
                 player.GetDamage(DamageClass.Ranged) += 0.03f;
             }
             else if (item.type == ModContent.ItemType<AquamarineRing>())
             {
-                player.GetDamage(ThoriumDamageBase<HealerDamage>.Instance).Flat -= 1f;
-
                 player.GetDamage(ThoriumDamageBase<HealerDamage>.Instance) += 0.03f;
             }
             else if (item.type == ModContent.ItemType<SapphireRing>())
             {
-                player.GetDamage(DamageClass.Magic).Flat -= 1f;
-
                 player.GetDamage(DamageClass.Magic) += 0.03f;
             }
             else if (item.type == ModContent.ItemType<AmethystRing>())
             {
                 player.GetArmorPenetration(DamageClass.Generic) += 1f;
             }
+			else if (item.type == ModContent.ItemType<DiamondRing>())
+            {
+                player.GetCritChance(DamageClass.Generic) += 2f;
+            }
             else if (item.type == ModContent.ItemType<TheRing>())
             {
-                player.GetDamage(DamageClass.Generic).Flat -= TheRing.FlatDamage;
-
                 player.GetArmorPenetration(DamageClass.Generic) += 1f;
                 player.GetDamage(DamageClass.Generic) += 0.04f;
             }
